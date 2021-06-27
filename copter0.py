cbea9acaefe87ba401a28e1d7877d452386c4861
faizH3 <20420026@student.binadarma.ac.id>
2021-06-27 08:58:19 -0400

    update

--- copter0.py
+++ copter0.py
@@ -1,14 +1,20 @@
+# pertama import module pygame
 import pygame
 
 pygame.init()
 
+# tentukan lebar,tinggi layar
 width, height = 800, 640
 
-
+# buat variabel untuk men setting layar
 screen = pygame.display.set_mode((width, height))
+
 
 background = (255,255,255)#RGB
 
+# ada beberapa cara penulisan melooping background
+# 1 dengan membuat fungsi
+# 2 langsung menempatkan code warna didalam looping
 def draw_bg(): #function
 	screen.fill(background)
 
@@ -16,6 +22,7 @@ run = True #data type boolean
 
 while run:
 	#call function draw_bg() and than you can look this in console
+	
 	draw_bg()
 
 	for event in pygame.event.get():
@@ -24,7 +31,7 @@ while run:
 			run = False
 
 		if event.type == pygame.KEYDOWN: # on press keyboards
-			if event. key == pygame.K_1:
+			if event.key == pygame.K_1:
 				exit()
 
 	pygame.display.update() #kita update layar
